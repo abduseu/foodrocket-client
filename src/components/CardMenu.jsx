798f9b7383f05f80524f34196b4cb0eb18ab98ac
sal400 <flarebd@gmail.com>
2024-03-28 17:20:22 +0600

    CardMenu ui updated

--- src/components/CardMenu.jsx
+++ src/components/CardMenu.jsx
@@ -1,21 +1,20 @@
-import { Link } from "react-router-dom";
-
 const CardMenu = ({ obj }) => {
     const { food_image, food_name, ingredients, price } = obj
-    console.log(obj)
+    
     return (
-        <Link to={`/`}>
-            <div className="flex justify-between items-center my-4">
-                <div className="flex items-center">
-                    <img src={food_image} className=" w-24" />
-                    <div>
-                        <h3 className="font-bold text-xl">{food_name}</h3>
-                        <p className="text-sm px-2">{ingredients}</p>
-                    </div>
+        <div className="flex justify-between items-center my-4">
+            <div className="md:flex items-center">
+                <img src={food_image} className=" w-24" />
+                <div>
+                    <h3 className="font-bold text-xl">{food_name}</h3>
+                    <p className="text-sm px-2">{ingredients}</p>
                 </div>
-                <h3 className="prim font-bold text-xl">${price}</h3>
             </div>
-        </Link>
+            <div className="flex items-center gap-2">
+                <h3 className="prim font-bold text-xl">${price}</h3>
+                <button className="btn btn-sm btn-circle btn-success text-xl text hover:bg-black">+</button>
+            </div>
+        </div>
     );
 };
 
