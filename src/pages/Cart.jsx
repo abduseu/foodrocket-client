bd8d95d14dc19abe7ce6da7709ff1ef81c5f5838
sal400 <flarebd@gmail.com>
2024-03-29 15:51:12 +0600

    handlePurchase functionality implemented on cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,14 @@
 import Swal from "sweetalert2";
 import useCart from "../hooks/useCart";
 import { axiosBase } from "../hooks/useAxios";
+import useAuth from "../hooks/useAuth";
 
 const Cart = () => {
+    const {user} = useAuth()
     const [cart, refetch] = useCart()
+    //const {_id, restaurantId, itemId, item_name, quantity, price } = cart
+
+    const totalPrice = cart.reduce((prev, current) => prev + parseFloat(current.price), 0);     //Calculate total price
 
     const handleDelete = (id) => {
         axiosBase.delete(`/cart/${id}`)
@@ -19,8 +24,32 @@ const Cart = () => {
             });
     }
 
-    // Calculate total price
-    const totalPrice = cart.reduce((prev, current) => prev + parseFloat(current.price), 0);
+    const handlePurchase = () => {
+        const orderData = {
+            userId: user.email,
+            restaurantId: cart[0].restaurantId,
+            items: cart.map(x => ({
+                itemId: x._id,
+                item_name: x.item_name,
+                quantity: 1,
+                price: x.price
+            })),
+            totalPrice: totalPrice.toFixed(2),
+            status: "pending",
+        };
+
+        axiosBase.post('/orders', orderData)
+            .then(res => {
+                if (res.data.insertedId) {
+                    Swal.fire(
+                        'Success!',
+                        'Your order has been placed successfully.',
+                        'success'
+                    );
+                }
+                refetch();  //Clear the cart
+            })
+    }
 
     return (
         <div>
@@ -45,8 +74,9 @@ const Cart = () => {
                                 </div>
                             )
                         }
-                        <div className="text-center">
-                            <h3 className="font-bold text-xl">Total: ${totalPrice.toFixed(2)}</h3>
+                        <div className="text-center py-5">
+                            <h3 className="font-bold text-xl pb-5">Total Price: <span>${totalPrice.toFixed(2)}</span></h3>
+                            <button onClick={handlePurchase} className="btn btn-success bg-prim text-white block mx-auto">Place order</button>
                         </div>
                     </div>
                 </div>
