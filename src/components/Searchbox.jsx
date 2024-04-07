b3ef8d3642a77f326fa3385fa9bebe9654514d39
sal400 <flarebd@gmail.com>
2024-04-08 02:23:19 +0600

    Searchbox ui updated

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -10,10 +10,10 @@ const Searchbox = () => {
     return (
         <div>
             <form className="bg-white flex rounded-lg">
-                <button className="btn btn-ghost btn-circle">
+                <div className="btn btn-ghost btn-circle prim">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="black"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                </button>
-                <input type="text" value={search} onChange={handleSearch} placeholder="Search" className="focus:outline-none rounded-lg w-full prim" autoComplete="off" />
+                </div>
+                <input type="text" value={search} onChange={handleSearch} placeholder="Search" className="focus:outline-none rounded-lg w-full text-black" autoComplete="off" />
             </form>
         </div>
     );
